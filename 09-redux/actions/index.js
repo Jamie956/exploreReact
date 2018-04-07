1f1ed31578480c87c01243c8fb62ddae9ca55bd1
jamie956 <752492509@qq.com>
2018-04-07 23:30:16 +0800

    update redux actions

--- 09-redux/actions/index.js
+++ 09-redux/actions/index.js
@@ -2,29 +2,29 @@
  * action types
  */
 
-export const ADD_TODO = 'ADD_TODO'
-export const TOGGLE_TODO = 'TOGGLE_TODO'
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+const ADD_TODO = 'ADD_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
 
 /*
  * action creators
  */
 
-export function addTodo(text) {
+function addTodo(text) {
   console.log('Call addTodo')
   console.log('arg: '+text)
   console.log({ type: ADD_TODO, text })
   return { type: ADD_TODO, text }
 }
 
-export function toggleTodo(index) {
+function toggleTodo(index) {
   console.log('Call toggleTodo')
   console.log('arg: '+index)
   console.log({ type: TOGGLE_TODO, index })
   return { type: TOGGLE_TODO, index }
 }
 
-export function setVisibilityFilter(filter) {
+function setVisibilityFilter(filter) {
   console.log('Call setVisibilityFilter')
   console.log('arg: '+filter)
   console.log({ type: SET_VISIBILITY_FILTER, filter })
