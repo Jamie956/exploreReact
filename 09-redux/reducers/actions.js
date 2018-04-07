782557ee7f4e39e7744118706ac999c0d8385990
jamie956 <752492509@qq.com>
2018-04-07 23:41:00 +0800

    update redux reducers

--- 09-redux/reducers/actions.js
+++ 09-redux/reducers/actions.js
@@ -21,30 +21,13 @@ export const VisibilityFilters = {
  */
 
 export function addTodo(text) {
-  console.log('Call addTodo')
-  console.log('arg: '+text)
-  console.log({ type: ADD_TODO, text })
   return { type: ADD_TODO, text }
 }
 
 export function toggleTodo(index) {
-  console.log('Call toggleTodo')
-  console.log('arg: '+index)
-  console.log({ type: TOGGLE_TODO, index })
   return { type: TOGGLE_TODO, index }
 }
 
 export function setVisibilityFilter(filter) {
-  console.log('Call setVisibilityFilter')
-  console.log('arg: '+filter)
-  console.log({ type: SET_VISIBILITY_FILTER, filter })
   return { type: SET_VISIBILITY_FILTER, filter }
 }
-
-/**
- * You see
- */
-
-// addTodo('It is addTodo')
-// toggleTodo('It is toggleTodo')
-// setVisibilityFilter('It is setVisibilityFilter')
