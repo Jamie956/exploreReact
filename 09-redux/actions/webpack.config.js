4c2bebe63bd7ff4c84155dbc8f535957f6093da2
jamie956 <752492509@qq.com>
2018-04-07 23:09:37 +0800

    update redux action

--- 09-redux/actions/webpack.config.js
+++ 09-redux/actions/webpack.config.js
@@ -2,24 +2,10 @@ var path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.jsx",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015', 'react', 'stage-0']
-          }
-        }
-      }
-    ]
   },
   plugins: [
     new HtmlWebpackPlugin()
