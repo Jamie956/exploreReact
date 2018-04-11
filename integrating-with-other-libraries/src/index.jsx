2c5b42393e4a05e6d74de04dffc0aa61faa96f7f
jamie <752492509@qq.com>
2018-04-11 13:28:28 +0800

    update

--- integrating-with-other-libraries/src/index.jsx
+++ integrating-with-other-libraries/src/index.jsx
@@ -17,38 +17,80 @@ import ReactDOM from "react-dom"
 
 //=====================================
 
-class Chosen extends React.Component {
-	componentDidMount() {
-	  this.$el = $(this.el);
-	  this.$el.chosen();
+// class Chosen extends React.Component {
+// 	componentDidMount() {
+// 		this.$el = $(this.el);
+// 		this.$el.chosen();
+// 	}
+
+// 	componentWillUnmount() {
+// 		this.$el.chosen('destroy');
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				<select className="Chosen-select" ref={el => this.el = el}>
+// 					{this.props.children}
+// 				</select>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// function Example() {
+// 	return (
+// 		<Chosen>
+// 			<option>vanilla</option>
+// 			<option>chocolate</option>
+// 			<option>strawberry</option>
+// 		</Chosen>
+// 	);
+// }
+
+// ReactDOM.render(
+// 	<Example />,
+// 	document.getElementById('root')
+// );
+
+//==============================
+
+class Example extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
 	}
-	
-	componentWillUnmount() {
-	  this.$el.chosen('destroy');
+
+	handleClick() {
+		$('#myModal').modal('show');
+		$('#nod').text('axim')
 	}
-	
+
 	render() {
-	  return (
-		<div>
-		  <select className="Chosen-select" ref={el => this.el = el}>
-			{this.props.children}
-		  </select>
-		</div>
-	  );
+		return (
+			<div>
+				<button ref={el => this.el = el} onClick={this.handleClick} >Show</button>
+				<div className="modal fade" ref="navBox" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+					<div className="modal-dialog" role="document">
+						<div className="modal-content">
+							<div className="modal-header">
+								<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+								<h4 className="modal-title" id="myModalLabel">Edit User</h4>
+							</div>
+							<div className="modal-body">
+								<div id="nod">hello</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
 	}
-  }
-  
-  function Example() {
-	return (
-	  <Chosen>
-		<option>vanilla</option>
-		<option>chocolate</option>
-		<option>strawberry</option>
-	  </Chosen>
-	);
-  }
-  
-  ReactDOM.render(
+}
+
+ReactDOM.render(
 	<Example />,
 	document.getElementById('root')
-  );
+);
