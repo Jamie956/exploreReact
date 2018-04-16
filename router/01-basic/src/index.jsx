21e0397d4b6369a19fdd8310a90fd362274dd2a7
jamie <752492509@qq.com>
2018-04-16 17:28:51 +0800

    update

--- router/01-basic/src/index.jsx
+++ router/01-basic/src/index.jsx
@@ -1,19 +1,14 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const Layout = () => (
   <BrowserRouter>
     <div>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
       </ul>
 
       <hr />
@@ -40,23 +35,15 @@ const About = () => (
 const Topics = ({ match }) => (
   <div>
     <h2>Topics</h2>
+    <h1>{match.url}</h1>
     <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
+      <li><Link to={`${match.url}/rendering`}>Rendering with React</Link></li>
+      <li><Link to={`${match.url}/components`}>Components</Link></li>
+      <li><Link to={`${match.url}/props-v-state`}>Props v. State</Link></li>
     </ul>
 
     <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
+    <Route exact path={match.url} render={() => <h3>Please select a topic.</h3>}
     />
   </div>
 );
@@ -67,4 +54,4 @@ const Topic = ({ match }) => (
   </div>
 );
 
-export default Layout;
+ReactDOM.render(<Layout />, document.getElementById('root'));
