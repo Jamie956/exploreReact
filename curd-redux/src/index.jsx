61c329fafd94eab7f06f5029ada4010fa2e7f969
jamie956 <752492509@qq.com>
2018-04-19 23:09:10 +0800

    init curd redux

---
+++ curd-redux/src/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(
+		applyMiddleware(thunk)
+	)
+);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
