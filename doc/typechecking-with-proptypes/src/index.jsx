1e0aa79b8c8a5ccaf194dd7e7bdd88f5a5d1b376
jamie <752492509@qq.com>
2018-04-19 16:26:45 +0800

    update

--- doc/typechecking-with-proptypes/src/index.jsx
+++ doc/typechecking-with-proptypes/src/index.jsx
@@ -2,61 +2,28 @@ import React, { Component } from 'react'
 import ReactDOM from "react-dom"
 import PropTypes from 'prop-types';
 
-// class Greeting extends React.Component {
-// 	render() {
-// 		return (
-// 			<h1>Hello, {this.props.name}</h1>
-// 		);
-// 	}
-// }
-
+// const Greeting = (props) => <h1>Hello, {props.name}</h1>
 // Greeting.propTypes = {
 // 	name: PropTypes.string
 // };
-
 // ReactDOM.render(<Greeting name={"tom"} />, document.getElementById('root'));
 
 //==================================
 
-// class MyComponent extends React.Component {
-// 	render() {
-// 		const children = this.props.children;
-// 		return (
-// 			<div>
-// 				{children}
-// 			</div>
-// 		);
-// 	}
-// }
-
+// const MyComponent = (props) => <div>{props.children}</div>
+// const App = () => <MyComponent><h1>Oops</h1></MyComponent>
 // MyComponent.propTypes = {
 // 	children: PropTypes.element.isRequired
 // };
-
-// ReactDOM.render(
-// <MyComponent>
-// 	<h1>Oops</h1>
-// </MyComponent>, 
-// document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 //=================================
 
-// class Greeting extends React.Component {
-// 	render() {
-// 		return (
-// 			<h1>Hello, {this.props.name}</h1>
-// 		);
-// 	}
-// }
-
+// const Greeting = (props) => <h1>Hello, {props.name}</h1>
 // Greeting.defaultProps = {
 // 	name: 'Stranger'
 // };
-
-// ReactDOM.render(
-// 	<Greeting />,
-// 	document.getElementById('root')
-// );
+// ReactDOM.render(<Greeting />, document.getElementById('root'));
 
 //==================================
 
@@ -72,7 +39,4 @@ class Greeting extends React.Component {
 	}
 }
 
-ReactDOM.render(
-	<Greeting />,
-	document.getElementById('root')
-);
+ReactDOM.render(<Greeting />, document.getElementById('root'));
