44874c1373c94b7853dd8d8431ece2222eca702e
jamie <752492509@qq.com>
2018-04-19 16:19:22 +0800

    update

--- doc/state-and-lifecycle/src/index.jsx
+++ doc/state-and-lifecycle/src/index.jsx
@@ -1,82 +1,29 @@
 import React from 'react'
 import ReactDOM from "react-dom";
 
-
-
-// function Clock(props) {
-// 	return (
-// 		<div>
-// 			<h1>Hello, world!</h1>
-// 			<h2>It is {props.date.toLocaleTimeString()}.</h2>
-// 		</div>
-// 	);
-// }
-// function tick() {
-// 	ReactDOM.render(
-// 		<Clock date={new Date()} />,
-// 		document.getElementById('root')
-// 	);
-// }
+// const Clock = () => <h2>{new Date().toLocaleTimeString()}</h2>
+// const tick = () => ReactDOM.render(<Clock />, document.getElementById('root'));
 // setInterval(tick, 1000);
-
-//======================================================
-
-// class Clock extends React.Component {
-// 	render() {
-// 		return (
-// 			<div>
-// 				<h1>Hello, world!</h1>
-// 				<h2>It is {this.props.date.toLocaleTimeString()}.</h2>
-// 			</div>
-// 		);
-// 	}
-// }
-// function tick() {
-// 	ReactDOM.render(
-// 		<Clock date={new Date()} />,
-// 		document.getElementById('root')
-// 	);
-// }
-// setInterval(tick, 1000);
-
-//======================================================
-
-// class Clock extends React.Component {
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = { date: new Date() };
-// 	}
-// 	render() {
-// 		return (
-// 			<div>
-// 				<h1>Hello, world!</h1>
-// 				<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-// 			</div>
-// 		);
-// 	}
-// }
-// ReactDOM.render(
-// 	<Clock />,
-// 	document.getElementById('root')
-// );
 
 //======================================================
 
 class Clock extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { date: new Date() };
+		this.state = {
+			date: new Date()
+		};
 	}
 	componentDidMount() {
-		this.timerID = setInterval(
-			() => this.tick(),
-			1000
-		);
+		console.log('call componentDidMount')
+		this.timerID = setInterval(() => this.tick(), 1000);
 	}
 	componentWillUnmount() {
+		console.log('call componentWillUnmount')
 		clearInterval(this.timerID);
 	}
 	tick() {
+		console.log('call tick')
 		this.setState({
 			date: new Date()
 		});
@@ -84,13 +31,9 @@ class Clock extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Hello, world!</h1>
-				<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+				<h2>{this.state.date.toLocaleTimeString()}</h2>
 			</div>
 		);
 	}
 }
-ReactDOM.render(
-	<Clock />,
-	document.getElementById('root')
-);
+ReactDOM.render(<Clock />, document.getElementById('root'));
