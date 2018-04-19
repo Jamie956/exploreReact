45520c4647d4da5216bea7226d95e31d075425d5
jamie <752492509@qq.com>
2018-04-19 11:42:03 +0800

    update

--- doc/handling-events/src/index.jsx
+++ doc/handling-events/src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from "react-dom";
 
-// class Toggle extends React.Component {
+// class App extends React.Component {
 // 	constructor(props) {
 // 		super(props);
-// 		this.state = { isToggleOn: true };
+// 		this.state = { 
+// 			isToggleOn: true 
+// 		};
 // 		this.handleClick = this.handleClick.bind(this);
 // 	}
 // 	handleClick() {
@@ -20,84 +22,65 @@ import ReactDOM from "react-dom";
 // 		);
 // 	}
 // }
-// ReactDOM.render(
-// 	<Toggle />,
-// 	document.getElementById('root')
-// );
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 //==============================================
 
-// class LoggingButton extends React.Component {
+// class App extends React.Component {
 // 	handleClick = () => {
 // 		console.log('this is:', this);
 // 	}
 // 	render() {
 // 		return (
-// 			<button onClick={this.handleClick}>
-// 				Click me
-// 		</button>
+// 			<button onClick={this.handleClick}>Click me</button>
 // 		);
 // 	}
 // }
-// ReactDOM.render(
-// 	<LoggingButton />,
-// 	document.getElementById('root')
-// );
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 //==============================================
 
-// class LoggingButton extends React.Component {
+// class App extends React.Component {
 // 	handleClick() {
 // 		console.log('this is:', this);
 // 	}
 // 	render() {
 // 		return (
-// 			<button onClick={(e) => this.handleClick(e)}>
-// 				Click me
-// 		</button>
+// 			<button onClick={(e) => this.handleClick(e)}>Click me</button>
 // 		);
 // 	}
 // }
-// ReactDOM.render(
-// 	<LoggingButton />,
-// 	document.getElementById('root')
-// );
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 //==============================================
 
-// class LoggingButton extends React.Component {
+// class App extends React.Component {
 // 	deleteRow(id, e) {
 // 		console.log(id);
 // 		console.log(e);
 // 	}
 
 // 	render() {
-// 		const id = '09das';
 // 		return (
-// 			<button onClick={(e) => this.deleteRow(id, e)}>Delete Row</button>
+// 			<button onClick={(e) => this.deleteRow('123', e)}>Delete Row</button>
 // 		);
 // 	}
 // }
-// ReactDOM.render(
-// 	<LoggingButton />,
-// 	document.getElementById('root')
-// );
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 //==============================================
 
-class LoggingButton extends React.Component {
+class App extends React.Component {
 	deleteRow(id, e) {
 		console.log(id);
 		console.log(e);
 	}
 	render() {
-		const id = '09das';
 		return (
-			<button onClick={this.deleteRow.bind(this, id)}>Delete Row</button>
+			<button onClick={this.deleteRow.bind(this, '123')}>Delete Row</button>
 		);
 	}
 }
-ReactDOM.render(
-	<LoggingButton />,
-	document.getElementById('root')
-);
+
+ReactDOM.render(<App />, document.getElementById('root'));
