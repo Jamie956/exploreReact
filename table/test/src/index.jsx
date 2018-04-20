2d44f8588c81538ca776f3895e481a359c9d8fe8
jamie956 <752492509@qq.com>
2018-04-20 22:08:36 +0800

    update

--- table/test/src/index.jsx
+++ table/test/src/index.jsx
@@ -1,101 +1,121 @@
 import React, { Component } from 'react'
 import ReactDOM from "react-dom";
 
-const getDisplayName = (el) => {
-  return el && el.type && (el.type.displayName || el.type.name);
-};
+// const getDisplayName = (el) => {
+//   return el && el.type && (el.type.displayName || el.type.name);
+// };
 
-const renderChangeRate = (changeRate) => {  };
+// const renderChangeRate = (changeRate) => {  };
 
-const renderThs = (columns) => {
-  return columns.map((col, index) => {
-    const { name, dataKey, th } = col.props;
-    const props = { name, dataKey, colIndex: index };
-    let content;
-    let className;
+// const renderThs = (columns) => {
+//   return columns.map((col, index) => {
+//     const { name, dataKey, th } = col.props;
+//     const props = { name, dataKey, colIndex: index };
+//     let content;
+//     let className;
 
-    if (React.isValidElement(th)) {
-      content = React.cloneElement(th, props);
-      className = getDisplayName(th);
-    } else if (_.isFunction(th)) {
-      content = th(props);
-    } else {
-      content = name || '';
-    }
+//     if (React.isValidElement(th)) {
+//       content = React.cloneElement(th, props);
+//       className = getDisplayName(th);
+//     } else if (_.isFunction(th)) {
+//       content = th(props);
+//     } else {
+//       content = name || '';
+//     }
 
-    return (
-      <th
-        key={`th-${index}`}
-        style={getStyle(col.props)}
-        className={`table-th col-${index} col-${dataKey} ${className || ''}`}
-      >
-        {content}
-      </th>
-    );
-  });
-};
+//     return (
+//       <th
+//         key={`th-${index}`}
+//         style={getStyle(col.props)}
+//         className={`table-th col-${index} col-${dataKey} ${className || ''}`}
+//       >
+//         {content}
+//       </th>
+//     );
+//   });
+// };
 
-const renderTds = (data, entry, columns, rowIndex) => {
-  return columns.map((col, index) => {
-    const { dataKey, td } = col.props;
-    const value = getValueOfTd(entry, dataKey);
-    const props = { data, rowData: entry, tdValue: value, dataKey, rowIndex, colIndex: index };
+// const renderTds = (data, entry, columns, rowIndex) => {
+//   return columns.map((col, index) => {
+//     const { dataKey, td } = col.props;
+//     const value = getValueOfTd(entry, dataKey);
+//     const props = { data, rowData: entry, tdValue: value, dataKey, rowIndex, colIndex: index };
 
-    let content;
-    let className;
-    if (React.isValidElement(td)) {
-      content = React.cloneElement(td, props);
-      className = getDisplayName(td);
-    } else if (td === 'changeRate') {
-      content = renderChangeRate(value || '');
-    } else if (_.isFunction(td)) {
-      content = td(props);
-    } else {
-      content = formatIndex(parseValueOfTd(value), dataKey, td);
-    }
+//     let content;
+//     let className;
+//     if (React.isValidElement(td)) {
+//       content = React.cloneElement(td, props);
+//       className = getDisplayName(td);
+//     } else if (td === 'changeRate') {
+//       content = renderChangeRate(value || '');
+//     } else if (_.isFunction(td)) {
+//       content = td(props);
+//     } else {
+//       content = formatIndex(parseValueOfTd(value), dataKey, td);
+//     }
 
-    return (
-      <td
-        key={`td-${index}`}
-        style={getStyle(col.props)}
-        className={`table-td col-${index} col-${dataKey} ${className || ''}`}
-      >
-        {content}
-      </td>
-    );
-  });
-};
+//     return (
+//       <td
+//         key={`td-${index}`}
+//         style={getStyle(col.props)}
+//         className={`table-td col-${index} col-${dataKey} ${className || ''}`}
+//       >
+//         {content}
+//       </td>
+//     );
+//   });
+// };
 
-const renderRows = (data, columns) => {
-  if (!data || !data.length) {return null;}
+// const renderRows = (data, columns) => {
+//   if (!data || !data.length) {return null;}
 
-  return data.map((entry, index) => {
-    return (
-      <tr className="table-tbody-tr" key={`tr-${index}`}>
-        {renderTds(data, entry, columns, index)}
-      </tr>
-    );
-  });
-};
+//   return data.map((entry, index) => {
+//     return (
+//       <tr className="table-tbody-tr" key={`tr-${index}`}>
+//         {renderTds(data, entry, columns, index)}
+//       </tr>
+//     );
+//   });
+// };
 
-function Table(props) {
-  const { children, data, className } = props;
-  const columns = findChildrenByType(children, Column);
+// function Table(props) {
+//   const { children, data, className } = props;
+//   const columns = findChildrenByType(children, Column);
 
+//   return (
+//     <div className={`table-container ${className || ''}`}>
+//       <table className="base-table">
+//         {hasNames(columns) && (
+//           <thead>
+//             <tr className="table-thead-tr">
+//               {renderThs(columns)}
+//             </tr>
+//           </thead>
+//         )}
+//         <tbody>{renderRows(data, columns)}</tbody>
+//       </table>
+//     </div>
+//   );
+// }
+
+const App = () =>{
   return (
-    <div className={`table-container ${className || ''}`}>
-      <table className="base-table">
-        {hasNames(columns) && (
-          <thead>
-            <tr className="table-thead-tr">
-              {renderThs(columns)}
-            </tr>
-          </thead>
-        )}
-        <tbody>{renderRows(data, columns)}</tbody>
-      </table>
-    </div>
-  );
+    <table>
+      <thead>
+        <th>name</th>
+        <th>age</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>tom</td>
+          <td>18</td>
+        </tr>
+        <tr>
+          <td>miko</td>
+          <td>28</td>
+        </tr>
+      </tbody>
+    </table>
+  )
 }
-
-ReactDOM.render(<Table />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
