702b62b118981f0493ae48d5e62f5e09e0ae83c0
jamie956 <752492509@qq.com>
2018-05-29 21:48:28 +0800

    update reduce

--- 09-redux/01-tmp/02/index.jsx
+++ 09-redux/01-tmp/02/index.jsx
@@ -1,28 +1,6 @@
 import { combineReducers, createStore } from "redux";
 
 /**
- * action types
- */
-// const SET_NAME = 'SET_NAME'
-// const SET_AGE = 'SET_AGE'
-// const ADD_ITEM = 'ADD_ITEM'
-
-/**
-//  * action creators
-//  */
-// const setNameActionCreator = (name) => {
-//   return { type: SET_NAME, payload: name }
-// }
-
-// const setAgeActionCreator = (age) => {
-//   return { type: SET_AGE, payload: age }
-// }
-
-// const addItemActionCreator = (item) => {
-//   return { type: ADD_ITEM, payload: item }
-// }
-
-/**
  * reducers
  */
 const userReducer = (state = {}, action) => {
