3b5f4a66946ebb699a5f434784fd6da28d796069
jamie956 <752492509@qq.com>
2018-05-31 00:23:47 +0800

    update

--- router/01-tmp/01/index.js
+++ router/01-tmp/01/index.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  withRouter,
+  Prompt
+} from "react-router-dom";
 
 const Home = () => <div>Home</div>;
 const About = () => <div>About</div>;
@@ -16,7 +22,7 @@ const App1 = () => (
   </BrowserRouter>
 );
 
-//
+//match.url
 const Art = () => <div>Art</div>;
 const Health = () => <div>Health</div>;
 const Topics = ({ match }) => {
@@ -42,4 +48,91 @@ const App2 = () => (
   </BrowserRouter>
 );
 
-ReactDOM.render(<App2 />, document.getElementById("root"));
+//params
+const App3 = () => (
+  <BrowserRouter>
+    <div>
+      <Link to="/itemid">getItem</Link> |
+      <Link to="/order/asc">asc</Link> |
+      <Link to="/order/desc">desc</Link>
+      <hr />
+      <Route path="/:id" component={Child} />
+      <Route
+        path="/order/:direction(asc|desc)"
+        component={ComponentWithRegex}
+      />
+    </div>
+  </BrowserRouter>
+);
+
+const Child = ({ match }) => <div>id: {match.params.id}</div>;
+
+const ComponentWithRegex = ({ match }) => (
+  <div>order: {match.params.direction}</div>
+);
+
+//withRouter
+const AuthButton = withRouter(({ history }) => (
+  <p>
+    <button onClick={() => history.push("/home")}>Sign out</button>
+  </p>
+));
+
+const App4 = () => (
+  <BrowserRouter>
+    <AuthButton />
+  </BrowserRouter>
+);
+
+//custom route
+const Protected = () => <h3>Protected</h3>;
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => <Component {...props} />} />
+);
+
+const App5 = () => (
+  <BrowserRouter>
+    <div>
+      <Link to="/protected">Protected Page</Link>
+      <PrivateRoute path="/protected" component={Protected} />
+    </div>
+  </BrowserRouter>
+);
+
+//custom link
+const App6 = () => (
+  <BrowserRouter>
+    <div>
+      <MyLink to="/" label="Home" />
+      <MyLink to="/about" label="About" />
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+  </BrowserRouter>
+);
+
+const MyLink = ({ label, to }) => <Link to={to}>{label}</Link>;
+
+//Prompt
+const App7 = () => (
+  <BrowserRouter>
+    <Link to="/">Form</Link>
+    <Route path="/" exact component={Form} />
+  </BrowserRouter>
+);
+
+class Form extends React.Component {
+  render() {
+    return (
+      <Prompt
+        when={true}
+        message={location =>
+          `Are you sure you want to go to ${location.pathname}`
+        }
+      />
+    );
+  }
+}
+
+ReactDOM.render(<App7 />, document.getElementById("root"));
