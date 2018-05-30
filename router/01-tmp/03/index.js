17780a99973d6099e8c17fe42ede8a922513acb2
jamie956 <752492509@qq.com>
2018-05-30 23:20:06 +0800

    update

--- router/01-tmp/03/index.js
+++ router/01-tmp/03/index.js
@@ -6,7 +6,7 @@ const fakeAuth = {
     isAuthenticated: false,
     authenticate(cb) {
         this.isAuthenticated = true;
-        setTimeout(cb, 100); // fake async
+        setTimeout(cb, 100);
     },
     signout(cb) {
         this.isAuthenticated = false;
