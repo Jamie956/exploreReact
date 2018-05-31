9862930fa474d4d2816f27f420474c9eb0bdae7f
jamie <jamie@justek.us>
2018-05-31 13:26:57 +0800

    remove

--- router/lazyLoad2/src/app/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const index = () => (
-    <div>
-        <h2>index</h2>
-    </div>
-);
-
-module.exports = index
--- router/lazyLoad2/src/app/list.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const list = () => (
-    <div>
-        <h2>list</h2>
-    </div>
-);
-
-module.exports = list
