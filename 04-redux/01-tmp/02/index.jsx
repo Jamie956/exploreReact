65c3ecd89402fd243c97d7bafd90c4957bb29ebe
jamie <jamie@justek.us>
2018-07-17 11:09:18 +0800

    update

--- 04-redux/01-tmp/02/index.jsx
+++ 04-redux/01-tmp/02/index.jsx
@@ -1,51 +1,46 @@
 import { combineReducers, createStore } from "redux";
 
-/**
- * reducers
- */
-const userReducer = (state = {}, action) => {
-	switch (action.type) {
-		case 'SET_NAME': {
-			return { ...state, name: action.payload };
-			break;
-		}
-		case 'SET_AGE': {
-			return { ...state, age: action.payload };
-			break;
-		}
-	}
-	return state;
-}
+//user储存对象
+const user = (state = {}, action) => {
+  switch (action.type) {
+    case "SET_NAME": {
+      return { ...state, name: action.payload };
+    }
+    case "SET_AGE": {
+      return { ...state, age: action.payload };
+    }
+  }
+  return state;
+};
 
-const itemReducer = (state = [], action) => {
-	switch (action.type) {
-		case 'ADD_ITEM': {
-			return state.concat({
-				id: Date.now(),
-				text: action.payload,
-			});
-			break;
-		}
-	}
-	return state;
-}
+//item储存对象(数组)
+const item = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_ITEM": {
+      return state.concat({
+        id: Date.now(),
+        text: action.payload
+      });
+    }
+  }
+  return state;
+};
 
-const rootReducer = combineReducers({
-	user: userReducer,
-	item: itemReducer
-})
+//定义根reducer,关联多个储存对象
+const rootReducer = combineReducers({ user, item });
 
-/**
- * store
- */
-const store = createStore(rootReducer)
+//关联根reducer
+const store = createStore(rootReducer);
 
-store.subscribe(() => {
-	console.log(store.getState());
-})
+//监听dispatch,调用时触发
+store.subscribe(() => console.log("Update"));
 
-store.dispatch({ type: 'SET_NAME', payload: 'Jamie' })
-store.dispatch({ type: 'SET_AGE', payload: 18 })
+//action,根据类型操作储存对象
+store.dispatch({ type: "SET_NAME", payload: "Jamie" });
+store.dispatch({ type: "SET_AGE", payload: 18 });
 
-store.dispatch({ type: 'ADD_ITEM', payload: 'FOO' })
-store.dispatch({ type: 'ADD_ITEM', payload: 'BAR' })
+store.dispatch({ type: "ADD_ITEM", payload: "FOO" });
+store.dispatch({ type: "ADD_ITEM", payload: "BAR" });
+
+//获取当前state
+console.log(store.getState());
