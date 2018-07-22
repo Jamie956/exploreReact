f6068096ed55af37a6b06926b2f2d59716f68fbe
jamie956 <752492509@qq.com>
2018-07-22 22:17:57 +0800

    update

--- semantic/src/index.jsx
+++ semantic/src/index.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import { Button } from "semantic-ui-react";
+import 'semantic-ui-css/components/button.min.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World.</h1>
         <Button>Click Here</Button>
       </div>
     );
--- semantic/webpack.config.js
+++ semantic/webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
             presets: ["es2015", "react", "stage-0"]
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
