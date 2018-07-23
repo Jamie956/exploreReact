392184849171f8322973d5f92a8e92828521927c
jamie <jamie@justek.us>
2018-07-23 12:45:55 +0800

    update

--- semantic/src/index.jsx
+++ semantic/src/index.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import { Button } from "semantic-ui-react";
-import 'semantic-ui-css/components/button.min.css'
+// import 'semantic-ui-css/components/button.min.css'
+import 'semantic-ui-css/components/button.css'
 
 class App extends Component {
   render() {
