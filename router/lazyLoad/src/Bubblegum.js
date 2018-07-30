35d7b52a92b54bc5a6e6ce434126d8594c52786a
jamie956 <752492509@qq.com>
2018-07-31 00:05:40 +0800

    update

--- router/lazyLoad/src/Bubblegum.js
+++
@@ -1,3 +0,0 @@
-import React, { Component } from "react";
-
-export const Bubblegum = () => <h2>Bubblegum</h2>;
--- router/lazyLoad/src/Home.js
+++
@@ -1,3 +0,0 @@
-import React, { Component } from "react";
-
-export const Home = () => <h2>Home</h2>;
