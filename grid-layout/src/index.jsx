22699753e3f1f608ac68b0476322df51396fbb5f
jamie <jamie@justek.us>
2018-08-01 16:47:16 +0800

    update

--- grid-layout/src/index.jsx
+++ grid-layout/src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
 import "./style.css";
-import App from "./05";
+import App from "./06";
 
 ReactDOM.render(<App />, document.getElementById("root"));
