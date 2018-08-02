b9380679007df69db8f81a837dc87bec00a0a387
jamie <jamie@justek.us>
2018-08-02 14:47:52 +0800

    update

--- ssr/02-initdata/src/server/index.js
+++ ssr/02-initdata/src/server/index.js
@@ -1,27 +1,21 @@
-import express from "express"
-import cors from "cors"
-import { renderToString } from "react-dom/server"
-import App from '../shared/App'
-import React from 'react'
-import serialize from "serialize-javascript"
+import express from "express";
+import { renderToString } from "react-dom/server";
+import App from "../shared/App";
+import React from "react";
+import serialize from "serialize-javascript";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-
-app.use(express.static("public"))
+app.use(express.static("dist"));
 
 app.get("*", (req, res, next) => {
-  const name = 'Tyler'
-  const markup = renderToString(
-    <App data={name} />
-  )
+  const name = "Tyler";
+  const markup = renderToString(<App data={name} />);
 
   res.send(`
     <!DOCTYPE html>
     <html>
       <head>
-        <title>SSR with RR</title>
         <script src="/bundle.js" defer></script>
         <script>window.__INITIAL_DATA__ = ${serialize(name)}</script>
       </head>
@@ -30,9 +24,7 @@ app.get("*", (req, res, next) => {
         <div id="app">${markup}</div>
       </body>
     </html>
-  `)
-})
+  `);
+});
 
-app.listen(3000, () => {
-  console.log(`Server is listening on port: 3000`)
-})
+app.listen(3000, console.log(`Listening on port: 3000`));
