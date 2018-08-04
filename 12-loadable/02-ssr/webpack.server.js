245ca4b3637fcaa3ba6f4b4a8dc7dce17106bf53
jamie956 <752492509@qq.com>
2018-08-04 19:47:38 +0800

    update

--- 12-loadable/02-ssr/webpack.server.js
+++ 12-loadable/02-ssr/webpack.server.js
@@ -1,45 +1,6 @@
 var path = require("path");
 var webpack = require("webpack");
 var nodeExternals = require("webpack-node-externals");
-// const { ReactLoadablePlugin } = require("react-loadable/webpack");
-
-// var clientConfig = {
-//   entry: "./05/client/index.js",
-//   output: {
-//     path: path.resolve(__dirname, "dist"),
-//     filename: "[name].js",
-//     chunkFilename: "[name]-[chunkhash:4].js",
-//     publicPath: "/dist/"
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.jsx?$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: "babel-loader",
-//           options: {
-//             presets: ["es2015", "react", "stage-0"]
-//             // plugins: ["react-loadable/babel", "syntax-dynamic-import"]
-//           }
-//         }
-//       }
-//     ]
-//   },
-//   plugins: [
-//     new webpack.DefinePlugin({
-//       __isClient__: "true"
-//     }),
-//     new ReactLoadablePlugin({
-//       filename: path.resolve(__dirname, "dist", "react-loadable.json")
-//     }),
-//     new webpack.optimize.CommonsChunkPlugin({
-//       name: "manifest",
-//       filename: "manifest.js",
-//       minChunks: Infinity
-//     })
-//   ]
-// };
 
 var server = {
   entry: "./05/server/index.js",
@@ -70,5 +31,4 @@ var server = {
   ]
 };
 
-// module.exports = [clientConfig, serverConfig];
 module.exports = server;
