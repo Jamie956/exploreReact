9ecc9c2811d2099ac629fc0156ccb629c2629279
jamie956 <752492509@qq.com>
2018-08-04 11:30:05 +0800

    update

--- loadable/02-ssr/01/client/index.js
+++ loadable/02-ssr/01/client/index.js
@@ -2,13 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "../shared/App";
 import Loadable from "react-loadable";
-import { BrowserRouter as Router } from "react-router-dom";
 
 Loadable.preloadReady().then(() => {
-  ReactDOM.hydrate(
-    <Router>
-      <App />
-    </Router>,
-    document.getElementById("root")
-  );
+  ReactDOM.hydrate(<App />, document.getElementById("root"));
 });
